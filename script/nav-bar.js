c9f87ba40c8792a81c514be2ce4bba99d377d6ff
Sagn1k <mesagnik@yahoo.com>
2017-12-28 00:14:55 +0530

    link needs to be added

--- script/nav-bar.js
+++ script/nav-bar.js
@@ -1,3 +1,9 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
 AFRAME.registerComponent('nav-bar', {
   schema: {
     on: {type: 'string'},
@@ -22,26 +28,3 @@ AFRAME.registerComponent('nav-bar', {
       }, data.dur);
     });
   },
-
-  /**
-   * Setup fade-in + fade-out.
-   */
-  setupFadeAnimation: function () {
-    var data = this.data;
-    var targetEl = this.data.target;
-
-    // Only set up once.
-    if (targetEl.dataset.setImageFadeSetup) { return; }
-    targetEl.dataset.setImageFadeSetup = true;
-
-    // Create animation.
-    targetEl.setAttribute('animation__fade', {
-      property: 'material.color',
-      startEvents: 'set-image-fade',
-      dir: 'alternate',
-      dur: data.dur,
-      from: '#FFF',
-      to: '#000'
-    });
-  }
-});
